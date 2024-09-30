a858da829656a9a7899404666bc16f233f132529
Frogbank <164952738+Frogbank@users.noreply.github.com>
2024-09-30 20:10:17 +1000

    Finished commenting code

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -11,37 +11,42 @@ public class MovingPlatform : MonoBehaviour
     public Vector3 target;
     private Vector3 startPos;
 
-    private void Start()
+    private void Start() // sets the point to return to
     {
         startPos = transform.position;
     }
 
     private void Update()
     {
-        if (move)
+        // moves towards the target position when the player is on the platform
+        if (move) 
         {
             transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime);
         }
-        else
+        else // moves back when the player is off the platform
         {
             transform.position = Vector3.MoveTowards(transform.position, startPos, Time.deltaTime);
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) // checks when the player gets on the platform
     {
         if(other.gameObject == player)
         {
-            player.transform.parent = this.transform;   
-            move = true;
+            player.transform.parent = this.transform; // sets the platform as the player's parent object
+
+
+            move = true; // gets the platform to move
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other) // checks when the player gets off the platform
     {
         if(other.gameObject == player)
         {
-            player.transform.parent = null;
+            player.transform.parent = null; // removes the platform as the player's parent
+
+
             move = false;
         }
     }
